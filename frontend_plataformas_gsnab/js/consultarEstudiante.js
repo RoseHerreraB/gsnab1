adb61d3fa33700ebd3935ace356bbf3d2460bea2
RoseHerreraB <110005780+RoseHerreraB@users.noreply.github.com>
2024-12-19 10:18:27 -0500

    Add files via upload
    frontend del proyecto  cargue masivo de información

---
+++ frontend_plataformas_gsnab/js/consultarEstudiante.js
@@ -0,0 +1,110 @@
+document.getElementById('consultarButton').addEventListener('click', async () => {
+    const numeroDocumento = document.getElementById('numeroDocumento').value;
+    const errorMessage = document.getElementById('errorMessage');
+    const resultTable = document.getElementById('resultTable');
+    const resultBody = document.getElementById('resultBody');
+
+    // Limpiar resultados anteriores
+    errorMessage.textContent = '';
+    resultBody.innerHTML = '';
+    resultTable.style.display = 'none';
+
+    if (!numeroDocumento) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ingresa un número de documento",
+
+        });
+        return;
+    }
+
+    const jsonData = JSON.stringify({ numero_documento: numeroDocumento });
+
+    try {
+        const response = await fetch('http://localhost/usuarios_plataformas_gsnab/apirest/consultar/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonData,
+        });
+
+        if (!response.ok) {
+            throw new Error('Error en la llamada a la API: ' + response.statusText);
+        }
+
+        const responseData = await response.json();
+
+        if (responseData.status === "success") {
+
+            let modifiedData = {};
+
+            Object.keys(responseData.data).forEach(key => {
+                let newKey = key;
+
+                // Aquí defines las claves que deseas cambiar
+                if (key === 'nombre_estudiante') {
+                    newKey = 'Student Name';
+                } else if (key === 'numero_identificacion') {
+                    newKey = 'ID Number';
+                } else if (key === 'nombre_curso') {
+                    newKey = 'Group';
+                } else if (key === 'nombre_estado') {
+                    newKey = 'State';
+                } else if (key === 'user_ARUKAY') {
+                    newKey = 'User: ARUKAY';
+                } else if (key === 'password_ARUKAY') {
+                    newKey = 'Password: ARUKAY';
+                } else if (key === 'user_FATHOM_CAMBRIDGE') {
+                    newKey = 'User: CAMBRIDGE';
+                } else if (key === 'password_CAMBRIDGE') {
+                    newKey = 'Password: CAMBRIDGE';
+                } else if (key === 'user_DELFOS') {
+                    newKey = 'User: DELFOS';
+                } else if (key === 'password_DELFOS') {
+                    newKey = 'Password: DELFOS';
+                } else if (key === 'user_FATHOM_READS') {
+                    newKey = 'User: FATHOM READS';
+                } else if (key === 'password_FATHOM_READS') {
+                    newKey = 'Password: FATHOM READS ';
+                } else if (key === 'user_MILTON_OCHOA') {
+                    newKey = 'user: MILTON OCHOA';
+                } else if (key === 'password_MILTON_OCHOA') {
+                    newKey = 'Password: MILTON OCHOA';
+                }
+
+                // Asignar el valor con la nueva clave
+                modifiedData[newKey] = responseData.data[key];
+            });
+
+            console.log(modifiedData);
+
+            // Llenar la tabla con los datos del estudiante
+            for (const [key, value] of Object.entries(modifiedData)) {
+                const row = document.createElement('tr');
+                const cellKey = document.createElement('td');
+                const cellValue = document.createElement('td');
+
+                cellKey.textContent = key;
+                cellValue.textContent = value;
+
+                row.appendChild(cellKey);
+                row.appendChild(cellValue);
+                resultBody.appendChild(row);
+            }
+            resultTable.style.display = 'table'; // Mostrar la tabla
+        } else {
+
+            Swal.fire({
+                icon: "error",
+                title: "No se encontró información",
+                text: "Verifica que el documento del estudiante este correcto."
+
+            });
+        }
+    } catch (error) {
+        console.error('Error al enviar datos a la API:', error);
+        errorMessage.textContent = 'Error al consultar el estudiante.';
+    }
+});
